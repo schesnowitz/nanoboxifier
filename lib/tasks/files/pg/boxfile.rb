d6fadffb6b1b7dac708bc7a065453418cf4fbf29
Steve Chesnowitz <steve@chesnowitz.com>
2018-03-02 09:23:17 -0500

    fixed yml for pg

--- lib/tasks/files/pg/boxfile.rb
+++ lib/tasks/files/pg/boxfile.rb
@@ -5,7 +5,7 @@ def open(url)
   Net::HTTP.get(URI.parse(url))
 end
 
-data = open('https://gist.githubusercontent.com/schesnowitz/73be37d1d9ce9e1127e3ed05833f692c/raw/2f3840dea67c18e1f739189755c7b7bce9980331/boxfile_pg.yml')
+data = open('https://gist.githubusercontent.com/schesnowitz/73be37d1d9ce9e1127e3ed05833f692c/raw/94daae47986f759b8f643ebe379e4f7659e71142/boxfile_pg.yml')
 
 filename = 'boxfile.yml'
 boxfile = File.new(filename, 'wb')
